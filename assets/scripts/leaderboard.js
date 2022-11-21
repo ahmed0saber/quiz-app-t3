3d43c256b8f66de43fe499b5d6231909720539ea
Ahmed Saber <ahmed0saber33@gmail.com>
2022-11-21 23:51:32 +0200

    init leaderboard js task

--- assets/scripts/leaderboard.js
+++ assets/scripts/leaderboard.js
@@ -1,1 +1,46 @@
-// EMPTY
+const getLeaderboardData = () => {
+    const data = [
+        {
+            user: "ahmed",
+            score: 100
+        },
+        {
+            user: "ali",
+            score: 95
+        },
+        {
+            user: "mohamed",
+            score: 92
+        },
+        {
+            user: "ashraf",
+            score: 90
+        },
+        {
+            user: "ebrahim",
+            score: 80
+        },
+        {
+            user: "said",
+            score: 75
+        },
+        {
+            user: "magdy",
+            score: 60
+        },
+        {
+            user: "lorem",
+            score: 50
+        },
+        {
+            user: "hi",
+            score: 25
+        },
+        {
+            user: "hello",
+            score: 10
+        },
+    ]
+
+    return data
+}
