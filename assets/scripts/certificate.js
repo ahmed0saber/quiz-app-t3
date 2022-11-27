d9aa88173a38156ce3e90042a2780d26cdeb1c8d
E1442020 <114053062+E1442020@users.noreply.github.com>
2022-11-27 15:20:36 +0200

    add social sharing

--- assets/scripts/certificate.js
+++ assets/scripts/certificate.js
@@ -1,22 +1,39 @@
 const sharingText = "I'm excited to share that I have passed HTML exam and got certified at Quiz App."
-const sharingUrl = "https://github.com/"
+ const sharingUrl = "https://github.com/"
+
 
 function shareToFacebook(){
-    window.open('http://facebook.com/sharer/sharer.php?quote='+sharingText+'&u='+encodeURIComponent(sharingUrl), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0')
+    window.open(`http://facebook.com/sharer/sharer.php?quote=${sharingText} &u= ${(sharingUrl)}`)
 }
 
 function shareToWhatsApp(){
-    window.open('https://api.whatsapp.com/send?text='+sharingText+' Test your skills now '+sharingUrl, '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0')
+    window.open(`https://api.whatsapp.com/send?text=${sharingText} Test your skills now ${sharingUrl}`)
 }
 
 function shareToLinkedIn(){
-    window.open('https://www.linkedin.com/sharing/share-offsite/?url='+encodeURIComponent(sharingUrl), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0')
+    window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${(sharingUrl)} &text=${sharingText} `)
 }
 
 function shareToTwitter(){
-    window.open('https://twitter.com/intent/tweet?text='+sharingText+' Test your skills now &url='+sharingUrl+'&hashtags=quiz_app,html', '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0')
+     window.open(`https://twitter.com/intent/tweet?text=${sharingText} Test your skills now &url=${sharingUrl}`)
 }
 
 function shareToTelegram(){
-    window.open('https://t.me/share/url?url='+sharingUrl+'&text='+sharingText, '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0')
+    window.open(`https://t.me/share/url?url=${sharingUrl} &text=${sharingText}`)
 }
+
+
+
+const certificateBtn =document.querySelector('.certificateBtn');
+const backBtn =document.querySelector('.backBtn');
+const faceBookBtn =document.querySelector('.faceBookBtn');
+const whatsAppBtn =document.querySelector('.whatsAppBtn');
+const linkedinBtn =document.querySelector('.linkedinBtn');
+const twitterBtn =document.querySelector('.twitterBtn');
+const telegramBtn =document.querySelector('.telegramBtn');
+
+faceBookBtn.addEventListener('click',shareToFacebook);
+twitterBtn.addEventListener('click',shareToTwitter);
+telegramBtn.addEventListener('click',shareToTelegram);
+linkedinBtn.addEventListener('click',shareToLinkedIn);
+whatsAppBtn.addEventListener('click',shareToWhatsApp);
